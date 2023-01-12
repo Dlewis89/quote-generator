5152625c2df44b64c8bd99ef5b9bdbbce0e35648
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2023-01-12 12:20:18 -0500

    finished project but initial commit

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log('Hello World');
+const longQuoteCharacterThreshold = 200;
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+function newQuote() {
+    showLoadingSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    authorText.textContent = quote.author ?? 'Unknown';
+
+    if (quote.text.length > longQuoteCharacterThreshold) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+}
+
+async function getQuotes() {
+    showLoadingSpinner();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+        removeLoadingSpinner();
+    } catch (err) {
+        showLoadingSpinner();
+        alert('Something went wrong');
+    }
+}
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+window.onload(getQuotes());
